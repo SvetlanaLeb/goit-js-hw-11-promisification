496b54e2a48c0fa72bd7ab3bcab406a396da4831
Svetlana Lebedkova <lebedkova.svetl@yandex.ru>
2021-07-04 16:40:16 +0300

    готовое дз

---
+++ src/js/task-2.js
@@ -0,0 +1,19 @@
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    return Promise.all(allUsers.map(user => {
+        
+        return Promise.resolve(user.name === userName ? { ...user, active: !user.active } : user)
+    }) )
+};
+
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
